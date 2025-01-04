8438b2874df4cad7dfd5227cff200ec5d1d38c38
nguyentrungtinh <trungtinhn300@gmail.com>
2025-01-04 20:22:15 +0700

    Init project automatic watering system backend iot

---
+++ src/main/java/com/TrungTinhBackend/automatic_watering_system_backend_iot/AutomaticWateringSystemBackendIotApplication.java
@@ -0,0 +1,13 @@
+package com.TrungTinhBackend.automatic_watering_system_backend_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticWateringSystemBackendIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticWateringSystemBackendIotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrungTinhBackend/automatic_watering_system_backend_iot/AutomaticWateringSystemBackendIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrungTinhBackend.automatic_watering_system_backend_iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticWateringSystemBackendIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
